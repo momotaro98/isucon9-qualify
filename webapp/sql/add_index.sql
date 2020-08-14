a938d1bbad121cb01a702ccf687a3d6ce469ccc8
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-08-14 18:53:21 +0900

    add composite index and update sql

--- webapp/sql/add_index.sql
+++ webapp/sql/add_index.sql
@@ -1,1 +1,3 @@
 create index items_created_at_index on items(created_at);
+create index items_seller_created_index on items(seller_id, created_at);
+create index items_buyer_created_index on items(buyer_id, created_at);
